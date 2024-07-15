9d3d3c7a4d19e90c07eb995de726cdcca15751b2
million-art <millionmulugeta09@gmail.com>
2024-07-15 11:15:53 +0300

    init

--- app/page.js
+++ app/page.js
@@ -1,23 +1,12 @@
 'use client'
-import { SDKProvider } from '@telegram-apps/sdk-react';
-import {
  
-  useViewport,
-  } from '@telegram-apps/sdk-react';
-import { useEffect } from 'react';
 import { retrieveLaunchParams } from '@telegram-apps/sdk';
 export default function Home() {
-  const vp = useViewport();
-
-useEffect(() => {
-  console.log(vp); // will be undefined and then Viewport instance.
-}, [vp]);
+ 
 const launchParams = retrieveLaunchParams();
 const fristname = launchParams.initData.user.firstName
 
   return (
-    <SDKProvider acceptCustomStyles debug>
-    <div>My application!{fristname}</div>
-  </SDKProvider>
-  );
+     <div>My application!{fristname}</div>
+   );
 }
