036fcc825a56415a7380e4018b17b71a8986e5cb
million-art <millionmulugeta09@gmail.com>
2024-07-15 10:02:44 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+ 
 import './App.css'
 import { retrieveLaunchParams } from '@telegram-apps/sdk';
 import {
@@ -21,8 +19,7 @@ useEffect(() => {
 
 const {  initData } = retrieveLaunchParams();
 function App() {
-  const [count, setCount] = useState(0)
-  const id = initData?.user?.id
+   const id = initData?.user?.id
   const user = initData?.user?.username
 
 
@@ -31,20 +28,11 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-
-      </div>
+       
+ 
       <p className="read-the-docs">
         Click on the Vite and React
       </p>
