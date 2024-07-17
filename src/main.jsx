184531cdbe74133cee92eb4ff9c57bf95ae8d7aa
million-art <millionmulugeta09@gmail.com>
2024-07-17 21:11:28 +0300

    fix main

--- src/main.jsx
+++ src/main.jsx
@@ -3,17 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { KonstaProvider, Block, Button } from 'konsta/react';
-import { store } from './app/store'
-import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <KonstaProvider theme="ios" >
 
-  <React.StrictMode>
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  </React.StrictMode>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
   </KonstaProvider>
 
 )
